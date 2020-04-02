b6f4782fe2108dafc73f99d6bfb0a3fc148283c9
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-04-02 13:11:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,7 +11,7 @@ class Song
     @artist = artist
     @genre = genre
     @@count += 1
-    @@artists << artist if !@@artists.include?(artist)
+    @@artists << artist unless @@artists.include?(artist)
     @@genres << genre if !@@genres.include?(genre)
   end
 
