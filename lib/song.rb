3f38fb5b86371ee84b31b56014959b1a8f87ffc0
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-04-02 13:11:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,7 +12,7 @@ class Song
     @genre = genre
     @@count += 1
     @@artists << artist unless @@artists.include?(artist)
-    @@genres << genre if !@@genres.include?(genre)
+    @@genres << genre unless @@genres.include?(genre)
   end
 
   def self.count
